7b48fd723aa52cc845ff53375f8ae67697f69e57
Ahmed <nahmed3536@gmail.com>
2023-12-27 18:07:17 +0400

    therapist safety

--- app.py
+++ app.py
@@ -17,6 +17,26 @@ config = toml.load(".streamlit/config.toml")
 
 
 ### AI ASSISTANT CODE ###
+# handling extreme dangerous or trickly situations by redirecting to speaking a human therapist
+SPEAK_TO_HUMAN = False
+SPEAK_TO_HUMAN_TEXT = "It appears you are discussing a very serious topic (i.e., suicide, inflict harm on your self and others, violence) and I recommend reaching out to a human therapist."
+def extreme_serious_input(prompt: str) -> bool:
+    """
+    It will analyze the prompt for any extreme serious key words and make discussion come to a close
+    """
+    keywords = [
+        "death", 
+        "suicide",
+        "harm",
+        "kill",
+        "attack",
+    ]
+    for keyword in keywords:
+        if keywords in prompt:
+            return True
+    return False
+
+
 def assistant(prompt: str, context: str = "You are a helpful assistant.") -> str:
     return "Not Programmed Yet!"
 
@@ -54,7 +74,17 @@ if prompt := st.chat_input():
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant", avatar=config["custom"]["assistant_avatar"]):
         with st.spinner("Thinking..."):
-            response = assistant(prompt, context)
+            # only check if response is dangerous if SPEAK_TO_HUMAN is false; otherwise default to seeking human therapist for every conversation afterwards
+            if SPEAK_TO_HUMAN == False:
+                SPEAK_TO_HUMAN = extreme_serious_input(prompt)
+            
+            # give AI assistant response if SPEAK_TO_HUMAN is false
+            if SPEAK_TO_HUMAN == True:
+                response = SPEAK_TO_HUMAN_TEXT
+            else:
+                response = custom_assistant(prompt, context)
+            
+            # write response
             st.write(response)
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
