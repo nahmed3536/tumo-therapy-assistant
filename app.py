058367f3da59c5b04411e10e202c1db7389d3bea
Ahmed <nahmed3536@gmail.com>
2023-12-27 17:58:13 +0400

    therapist welcome message

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ st.set_page_config(page_title="Arpi Therapy")
 # set the header
 st.header("Arpi Therapy")
 
-### PAGE CONFIGURATION ###
+
+
+### CORE FUNCTIONALITY ###
+# access toml configuration
+import toml
+config = toml.load(".streamlit/config.toml")
+
+
 
 ### AI ASSISTANT CODE ###
 def assistant(prompt: str, context: str = "You are a helpful assistant.") -> str:
@@ -15,17 +22,26 @@ def assistant(prompt: str, context: str = "You are a helpful assistant.") -> str
 
 context = ""
 
-### AI ASSISTANT CODE ###
+
 
 
 ### WEBSITE CODE ###
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+    st.session_state.messages = [
+        {
+            "role": "assistant", 
+            "content": (
+                "Hello, I'm Arpi, your AI therapist! "
+                "I'm here to support you on your journey on "
+                "mental well-being. How are you feeling today? "
+            )
+        }
+    ]
 
 # Display chat messages
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(message["role"], avatar=config["custom"]["assistant_avatar"]):
         st.write(message["content"])
 
 # User-provided prompt
@@ -36,10 +52,9 @@ if prompt := st.chat_input():
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar=config["custom"]["assistant_avatar"]):
         with st.spinner("Thinking..."):
             response = assistant(prompt, context)
             st.write(response)
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
-### WEBSITE CODE ###
